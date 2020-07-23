c0090074bd977d9ac03d30094fb83c5ec92b308a
AlbertoParente <albertoparente20@gmail.com>
2020-07-23 11:35:12 -0300

    Adding database settings

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: 'vls021130',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
