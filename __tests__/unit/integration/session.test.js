009ce04b2eb4f342b4bb3204cbb56103d8f40e33
AlbertoParente <albertoparente20@gmail.com>
2020-07-26 23:27:37 -0300

    Creating a simple test and configuring .env file

---
+++ __tests__/unit/integration/session.test.js
@@ -0,0 +1,9 @@
+describe('Autetication', () => {
+    it('should sum two numbers', () => {
+        const x = 2
+        const y = 4
+
+        const sum = x + y
+        expect(sum).toBe(7)
+    })
+})
