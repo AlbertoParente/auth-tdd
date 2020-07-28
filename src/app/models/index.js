3809b7e3b139e7ae37ec2c6a6b2d77e2b86d26f6
AlbertoParente <albertoparente20@gmail.com>
2020-07-27 23:13:03 -0300

    Removing bug fixed

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -7,15 +7,21 @@ const basename = path.basename(__filename);
 const config = require('../../config/database');
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(
+  config.database, 
+  config.username, 
+  config.password, 
+  config
+);
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const routes = require('express').Router()
+const routes = require("express").Router()
 const { User } = require('./app/models')
 
 User.create({ 
