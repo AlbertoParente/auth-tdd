fcff4cf6bed53fab91040f62d85c46d7755bcf45
AlbertoParente <albertoparente20@gmail.com>
2020-07-31 20:49:52 -0300

    Creating first valid unit test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,15 +1,20 @@
+const request = require('supertest')
+const app = require('../../src/app')
 const { User } = require('../../src/app/models')
 
 describe('Autetication', () => {
-    it('should sum two numbers', async () => {
-      const user = await User.create({
-        name: 'Alberto',
-        email: 'albertoparentefh@gmail.com',
-        password_hash: '123456789'
-      })
-
-      console.log(user)
-
-      expect(user.email).tobe('albertoparentefh@gmail.com')
+  it('should auteticate with valid credentials', async() => {
+    const user = await User.create({
+      name: 'Alberto',
+      email: 'albertoparentefh@gmail.com',
+      password_hash: '123456789'
     })
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123456789'
+      })
+    expect(response.status).toBe(200)
+  })
 })
