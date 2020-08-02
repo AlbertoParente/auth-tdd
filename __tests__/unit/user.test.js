6a05f2230813d6cb2e994e8c04efcad8758d27c7
AlbertoParente <albertoparente20@gmail.com>
2020-08-02 17:54:55 -0300

    Removing bug fix (Compare hash)

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -4,17 +4,17 @@ const truncate = require('../utils/truncate')
 
 describe('User', () => {
   beforeEach(async () => {
-    await truncate
+    await truncate()
   })
 
   it('should encrypt user password', async () => {
     const user = await User.create({
       name: 'Alberto',
       email: 'albertoparentefh@gmail.com',
-      passoword: '123456'
+      password: '123456'
     })
 
-    const compareHash = await bcrypt.compare('123456', user.passoword_hash)
+    const compareHash = await bcrypt.compare('123456', user.password_hash)
 
     expect(compareHash).toBe(true)
   })
