001657ed33a36b3059276d1bc08c137d5e4fcbfc
AlbertoParente <albertoparente20@gmail.com>
2020-08-02 22:31:47 -0300

    Creting check password test

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -24,4 +24,21 @@ describe('Autetication', () => {
 
     expect(response.status).toBe(200)
   })
+
+  it('should not authenticate with invalid credentials', async () => {
+    const user = await User.create({
+      name: 'Alberto',
+      email: 'albertoparentefh@gmail.com',
+      password: '123123'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123456'
+      })
+
+    expect(response.status).toBe(401)
+  })
 })
